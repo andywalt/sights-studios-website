e37ad4d3a439b23fef1346e220d3537b33e9e255
Andy Walt <andywalters42@gmail.com>
2026-05-13 13:41:25 -0500

    feat: wire AIRescueSection into homepage between hero and features

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Navbar from "../components/Navbar/navBar.js";
 import HeroSection from '../components/HomeSections/HeroSection/heroSection';
-import CardCarousel from "../components/CardCarousel/CardCarousel.js";
+import AIRescueSection from '../components/HomeSections/AIRescueSection/AIRescueSection';
 import FeatureSection from '../components/HomeSections/FeatureSection/featureSection';
+import CardCarousel from "../components/CardCarousel/CardCarousel.js";
 import ContactSection from "../components/HomeSections/ContactSection/ContactSection.js";
 
 function HomePage() {
@@ -10,11 +11,12 @@ function HomePage() {
     <div className="HomePage">
       <Navbar />
       <HeroSection />
+      <AIRescueSection />
       <FeatureSection />
       <CardCarousel />
       <ContactSection />
     </div>
   );
-};
+}
 
 export default HomePage;
